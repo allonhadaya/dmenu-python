b3b392646b8f763145354f52100d853e6a217074
Allon Hadaya <self@allon.nyc>
2017-02-04 11:08:59 -0500

    Patch update 0.2.0 -> 0.2.1.

--- dmenu/__init__.py
+++ dmenu/__init__.py
@@ -4,4 +4,4 @@ dmenu is a dynamic menu for X, originally designed for dwm. It manages large num
 
 from .dmenu import show, version, DmenuError, DmenuCommandError, DmenuUsageError
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name='dmenu',
-    version='0.2.0',
+    version='0.2.1',
     description='A dmenu wrapper.',
     long_description=long_description,
     url='https://github.com/allonhadaya/dmenu',
